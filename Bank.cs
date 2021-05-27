d60dc7f62ed47aa87c61ad2f35773446433ae8c8
petrik technikum <info@petrik.hu>
2021-05-27 15:19:56 +0200

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProjekt
+{
+    class Bank
+    {
+        private class Szamla
+        {
+            private string nev;
+            private string szamlaszam;
+            private ulong egyenleg;
+            public string Nev
+            {
+                get
+                {
+                    return nev;
+                }
+            }
+            public string Szamlaszam
+            {
+                get
+                {
+                    return szamlaszam;
+                }
+            }
+            public ulong Egyenleg
+            {
+                get
+                {
+                    return egyenleg;
+                }
+                set
+                {
+                    egyenleg = value;
+                }
+            }
+            public Szamla(string nev, string szamlaszam)
+            {
+                this.nev = nev;
+                this.szamlaszam = szamlaszam;
+                egyenleg = 0;
+            }
+        }
+        private List<Szamla> szamlak = new List<Szamla>();
+        private Szamla SzamlaKeres(string szamlaszam)
+        {
+            foreach (Szamla szamla in szamlak)
+            {
+                if (szamla.Szamlaszam == szamlaszam)
+                {
+                    return szamla;
+                }
+            }
+            throw new HibasSzamlaszamException(szamlaszam);
+        }
+        // Egy létező számlára pénzt helyez
+        public void EgyenlegFeltolt(string szamlaszam, ulong osszeg)
+        {
+            SzamlaKeres(szamlaszam).Egyenleg += osszeg;
+        }
+
+        // Új számlát nyit a megadott névvel, számlaszámmal
+        public void UjSzamla(string nev, string szamlaszam)
+        {
+            if (szamlak.Count > 0)
+            {
+                foreach (Szamla szamla in szamlak)
+                {
+                    if (szamla.Szamlaszam == szamlaszam)
+                    {
+                        throw new ArgumentException(
+                            "A megadott számlaszámmal már létezik számla", "szamlaszam");
+                    }
+                }
+            }
+            szamlak.Add(new Szamla(nev, szamlaszam));
+        }
+
+        // Két számla között utal.
+        // Ha nincs elég pénz a forrás számlán, akkor
+        public bool Utal(string honnan, string hova, ulong osszeg)
+        {
+            Szamla egyik = SzamlaKeres(honnan);
+            Szamla masik = SzamlaKeres(hova);
+            if (egyik == masik)
+            {
+                throw new ArgumentException("A két számlaszám megyegyezik!");
+            }
+            if (egyik.Egyenleg < osszeg)
+            {
+                return false; 
+            }
+            egyik.Egyenleg -= osszeg;
+            masik.Egyenleg += osszeg;
+            return true;
+        }
+
+        // Lekérdezi az adott számlán lévő pénzösszeget
+        public ulong Egyenleg(string szamlaszam)
+        {
+            return SzamlaKeres(szamlaszam).Egyenleg;
+        }
+    }
+
+    // Nem létező számla esetén dobhatjuk bármely függvényből
+    class HibasSzamlaszamException : Exception
+    {
+        public HibasSzamlaszamException(string szamlaszam)
+            : base("Hibas szamlaszam: " + szamlaszam)
+        {
+        }
+    }
+}
---
+++ Tests/BankTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BankProjekt.Tests
+{
+    [TestFixture]
+    class BankTest
+    {
+        Bank b;
+
+        [SetUp]
+        public void Setup()
+        {
+            b = new Bank();
+        }
+
+        [TestCase]
+        public void UjjSzamlaHibaNelkulLetrejon()
+        {
+            Assert.DoesNotThrow(() => b.UjSzamla("Teszt Elek", "1234"));
+        }
+
+        [TestCase]
+        public void UjjSzamlaDuplikaltSzamlaszam()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<ArgumentException>(() => b.UjSzamla("Kovács Elek", "1234"));
+        }
+
+        [TestCase]
+        public void UjszamlaLetezoNevvelNincsHiba()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.DoesNotThrow(() => b.UjSzamla("Teszt Elek", "4321"));
+        }
+
+        [TestCase]
+        public void EgyenlegFeltoltNemLetezoEgyenleg()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<HibasSzamlaszamException>(() => b.EgyenlegFeltolt("Kenyér", 10000));
+        }
+
+        [TestCase]
+        public void EgyenlegFeltoltMegfeleloErtekek()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.DoesNotThrow(() => b.EgyenlegFeltolt("1234", 10000));
+        }
+
+        [TestCase]
+        public void EgyenlegLekerdezNemLetezoEgyenleg()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<HibasSzamlaszamException>(() => b.Egyenleg("Kenyér"));
+        }
+
+        [TestCase]
+        public void EgyenlegLekerdezMegfeleloErtekek()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.AreEqual(0, EgyenlegszerzoTeszthez(b, "1234"));
+        }
+        private ulong EgyenlegszerzoTeszthez(Bank b, string szamla)
+        {
+            return b.Egyenleg(szamla);
+        }
+
+        [TestCase]
+        public void UtalasElsoSzamlaNemJo()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<HibasSzamlaszamException>(() => b.Utal("Kenyér", "1234", 0));
+        }
+
+        [TestCase]
+        public void UtalasMasodikSzamlaNemJo()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<HibasSzamlaszamException>(() => b.Utal("1234", "Kenyér", 0));
+        }
+
+        [TestCase]
+        public void UtalasASzamlakMegyegyeznek()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            Assert.Throws<ArgumentException>(() => b.Utal("1234", "1234", 0));
+        }
+
+        [TestCase]
+        public void UtalasNincsEgyenleg()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            b.UjSzamla("Teszt Béla", "4321");
+            Assert.IsFalse(UtalasEllenorzoTeszthez(b));
+        }
+        private bool UtalasEllenorzoTeszthez(Bank b)
+        {
+            return b.Utal("1234", "4321", 200);
+        }
+
+        [TestCase]
+        public void UtalasMegfeleloErtekekkel()
+        {
+            b.UjSzamla("Teszt Elek", "1234");
+            b.UjSzamla("Teszt Béla", "4321");
+            b.EgyenlegFeltolt("1234", 200);
+            Assert.IsTrue(UtalasEllenorzoTeszthez(b));
+        }
+    }
+}
